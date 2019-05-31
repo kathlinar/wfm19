73f8fde9033df6d09c4ffb7f012eea5af34b45a6
fichtip <patrickfichtinger@gmx.at>
2019-05-31 21:46:08 +0200

    allow h2 jdbc connections

--- src/main/java/wfm/group3/localy/WfmApplication.java
+++ src/main/java/wfm/group3/localy/WfmApplication.java
@@ -3,10 +3,14 @@ package wfm.group3.localy;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableProcessApplication
@@ -22,5 +26,16 @@ public class WfmApplication {
     @EventListener
     private void processPostDeploy(PostDeployEvent event) {
         //runtimeService.startProcessInstanceByKey("loanApproval");
+    }
+
+    /**
+     * Start internal H2 server so we can query the DB from IDE
+     *
+     * @return H2 Server instance
+     * @throws SQLException
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
